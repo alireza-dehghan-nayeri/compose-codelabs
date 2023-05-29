57e4bf4b82b1c08e7d211e92a725ea6ce1f89089
alireza.dehghan <alireza79.nayeri@gmail.com>
2023-05-29 11:23:36 +0330

    * 12-State in ViewModel

--- app/src/main/java/com/example/basicstatecodelab/ui/view/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/ui/view/WellnessScreen.kt
@@ -2,20 +2,26 @@ package com.example.basicstatecodelab.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import com.example.basicstatecodelab.ui.view.model.WellnessTask
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.basicstatecodelab.ui.view.model.WellnessTasksList
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
     Column(modifier = modifier) {
         StatefulCounter()
 
-        val list = remember { getWellnessTasks().toMutableStateList() }
-        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
     }
 }
-
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
--- app/src/main/java/com/example/basicstatecodelab/ui/view/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicstatecodelab/ui/view/WellnessTaskItem.kt
@@ -1,37 +1,17 @@
 package com.example.basicstatecodelab.ui.view
 
-import androidx.compose.material.icons.Icons
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-
-@Composable
-fun WellnessTaskItem(
-    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
-) {
-    var checkedState by rememberSaveable { mutableStateOf(false) }
-
-    WellnessTaskItem(
-        taskName = taskName,
-        checked = checkedState,
-        onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = onClose,
-        modifier = modifier,
-    )
-}
-
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessTaskItem(
@@ -59,4 +39,5 @@ fun WellnessTaskItem(
         }
     }
 }
+
 
