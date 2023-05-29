6745f09502b6efe99d0bfe0b50194fad2a12ae05
alireza.dehghan <alireza79.nayeri@gmail.com>
2023-05-29 10:56:54 +0330

    * 11-Observable MutableList

--- app/src/main/java/com/example/basicstatecodelab/ui/view/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/ui/view/WellnessScreen.kt
@@ -2,14 +2,20 @@ package com.example.basicstatecodelab.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import com.example.basicstatecodelab.ui.view.model.WellnessTask
 import com.example.basicstatecodelab.ui.view.model.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTasksList()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
 
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
